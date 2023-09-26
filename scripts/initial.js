c752b72b4d921584e9e13daeba2c566ad93f726c
pashaBy94 <pascom_by@mail.ru>
2023-09-26 19:56:17 +0300

    game 09

--- scripts/initial.js
+++ scripts/initial.js
@@ -1,3 +1,14 @@
-function openStartMenu(){
-    
+function openSettings(ev){
+    const modal = document.querySelector('.modal_settings');
+    console.log(modal);
+    modal.classList.add('open');
+    modal.classList.remove('close')
+
+}
+function closeSettings(){
+    const modal = document.querySelector('.modal_settings');
+    console.log(modal);
+    modal.classList.add('close')
+    modal.classList.remove('open')
+
 }
